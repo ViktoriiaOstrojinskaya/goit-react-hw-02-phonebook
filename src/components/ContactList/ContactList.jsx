fcacddd4fc8ed64f317e9158d219cdc1fef9e54a
Viktoriia Ostrojinskaya <viktoriia.ostrojinskaya@gmail.com>
2022-12-02 21:53:26 -0500

    start

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,10 @@
+export const ContactList = () => (
+  <ul>
+    {/* {this.state.contacts.map(contact => (
+      <li key={contact.id}>
+        <p>{contact.name}</p>
+        <p>{contact.number}</p>
+      </li>
+    ))} */}
+  </ul>
+);
---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,1 @@
+export const Filter = () => <input type="text" />;
